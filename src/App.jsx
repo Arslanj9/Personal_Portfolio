c7815ed646fe699bc4a83e079c93d025fe4070b3
Arslan Javaid <aja118379@gmail.com>
2024-06-23 13:02:55 +0500

    Provided basic setup for the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const  App = () => {
 
   return (
     <>
+    <Router>
       <h1>Hello worlds</h1>
+    </Router>
     </>
   )
 }
