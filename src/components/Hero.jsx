724c8ab19423ee5a0e4604cbff4f3956566e9e86
Arslan Javaid <aja118379@gmail.com>
2024-06-24 18:44:47 +0500

    Updating Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,9 +8,9 @@ const Hero = () => {
     <section className={`w-full h-screen flex`}>
       {/* Text */}
       <div
-        className={`flex-1 basis-2/10 inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`flex-1 basis-2/10 top-[120px] max-w-7xl h-100 ${styles.paddingX} flex flex-col justify-center ml-12  items-start gap-16`}
       >
-        <div>
+        
           <h1 className={`${styles.heroHeadText} text-white h-24`}>
             Hi, I'm <span className='text-[#915EFF]'>Arslan</span>
           </h1>
@@ -18,7 +18,7 @@ const Hero = () => {
             I develop 3D visuals, user <br className='sm:block hidden' />
             interfaces and web applications
           </p>
-        </div>
+        
       </div>
 
       {/* 3D Model */}
