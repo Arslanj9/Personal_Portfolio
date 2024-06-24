db295375a4b4949898220cbe6cab0785347003fb
Arslan Javaid <aja118379@gmail.com>
2024-06-24 18:30:21 +0500

    Updating Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,16 +5,16 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`w-full h-screen`}>
+    <section className={`w-full h-screen flex`}>
       {/* Text */}
       <div
-        className={`inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`flex-1 basis-2/10 inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div>
           <h1 className={`${styles.heroHeadText} text-white h-24`}>
             Hi, I'm <span className='text-[#915EFF]'>Arslan</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          <p className={`${styles.heroSubText} mt-2 text-white-100 h-24`}>
             I develop 3D visuals, user <br className='sm:block hidden' />
             interfaces and web applications
           </p>
@@ -22,7 +22,7 @@ const Hero = () => {
       </div>
 
       {/* 3D Model */}
-      <ComputersCanvas />
+      <ComputersCanvas className="flex-1 basis-8/10"/>
 
       {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
